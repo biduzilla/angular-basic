b391057350e875a5428b6f6c562e48da454425d3
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2023-11-10 15:09:06 -0300

    projeto finalizado

--- basic/src/app/shooping-list/shooping-list.component.ts
+++ basic/src/app/shooping-list/shooping-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shooping-list',
   templateUrl: './shooping-list.component.html',
   styleUrls: ['./shooping-list.component.css']
 })
-export class ShoopingListComponent {
+export class ShoopingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple',5),
+    new Ingredient('Tomato',5),
+  ];
 
+  constructor(){}
+  ngOnInit() {}
 }
